d1b0de89c94af1e84fb56d0d2c58b6622b384cc7
jon-jc <jonathancho.jc@gmail.com>
2024-03-04 13:22:59 -0800

    feat: added scrollable area in the sidebar, servers with icons now appear in sidebar
    Add scroll area and user button components to navigation sidebar

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+import { ActionTootip } from "@/components/action-tooltip";
+
+interface NavigationItemProps {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+
+  return (
+    <ActionTootip side="right" align="center" label={name}>
+      <button onClick={() => {}} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[36px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[60px] w-[60px] rounded-[30px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt="Channel" />
+        </div>
+      </button>
+    </ActionTootip>
+  );
+};
