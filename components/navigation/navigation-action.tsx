9af519cd0bb0b0956e8153d68f0d34041eddd69d
jon-jc <jonathancho.jc@gmail.com>
2024-03-04 13:45:05 -0800

    feat: create new servers using navbar
    Add ModalProvider and useModal hook***
    ***Update ModeToggle button styling***
    ***Add onClick event to NavigationItem button***
    ***Update package.json with zustand dependency

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,12 +2,18 @@
 import { Plus } from "lucide-react";
 
 import { ActionTootip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 export const NavigationAction = () => {
+  const { onOpen } = useModal();
+
   return (
     <div>
       <ActionTootip side="right" align="center" label="Create server">
-        <button className="group flex items-center">
+        <button
+          onClick={() => onOpen("createServer")}
+          className="group flex items-center"
+        >
           <div
             className="flex mx-3 h-[60px] w-[60px] rounded-[30px] group-hover:rounded-[16px] transition-all
             overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-cyan-500"
