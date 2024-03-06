7c2742204923833dfbd03d1f449e4c6644838087
jon-jc <jonathancho.jc@gmail.com>
2024-03-06 12:52:45 -0800

    Add ChatInput component to ChannelIdPage

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -41,6 +42,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/nessages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
+import { PlusCircleIcon, SmilePlusIcon } from "lucide-react";
+import { Input } from "../ui/input";
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: "conversation" | "channel";
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                    className="absolute top-7 left-8 h-[24px] w-[24px]
+                            bg-slate-500 dark:bg-slate-400 hover:bg-slate-600 dark:hover:bg-slate-300 transition
+                            rounded-full flex items-center justify-center p-1"
+                  >
+                    <PlusCircleIcon className="w-5 h-5 text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    disabled={isLoading}
+                    className="px-14 py-6 bg-slate-200/90 dark:bg-slate-700/75 border-none
+                  focus-visible:ring-0 focus-visible:ring-offset-0 text-slate-600 dark:text-slate-200"
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                    {...field}
+                  />
+                  <div className="absolute top-7 right-8">
+                    <SmilePlusIcon />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        ></FormField>
+      </form>
+    </Form>
+  );
+};
+
+export default ChatInput;
