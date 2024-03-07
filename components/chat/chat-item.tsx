d65e81ac9ceb7d0c944d436f84c6f442259f92fd
jon-jc <jonathancho.jc@gmail.com>
2024-03-07 10:52:14 -0800

    Add editing and deleting functionality to ChatItem component

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { Member, Profile } from "@prisma/client";
+import { Member, MemberRole, Profile } from "@prisma/client";
 import UserAvatar from "../user-avatar";
 import { ActionTootip } from "../action-tooltip";
-import { ShieldPlusIcon } from "lucide-react";
+import { Edit2Icon, ShieldPlusIcon, Trash2Icon, X } from "lucide-react";
 import { BadgeCheckIcon } from "lucide-react";
+import Image from "next/image";
+import { FileIcon } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface ChatItemProps {
   id: string;
@@ -39,6 +43,17 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const fileType = fileUrl?.split(".").pop();
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.id === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
   return (
     <div className="relative group flex items-center hover: bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
@@ -59,9 +74,78 @@ export const ChatItem = ({
               {timestamp}
             </span>
           </div>
-          {content}
+          {isImage && (
+            <a
+              href={fileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative aspect-square rounded-md overflow-hidden mt-2 border flex items-center bg-secondary w-48 h-48"
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                className="object-cover"
+              />
+            </a>
+          )}
+          {isPDF && (
+            <div className="">
+              <div
+                className="relative flex items-center p-2 mt-2 rounded-md
+        bg-background/10"
+              >
+                <FileIcon className="h-10 w-10 fill-cyan-200 stroke-cyan-400" />
+                <a
+                  href={fileUrl}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="ml-2 text-sm text-cyan-500 dark:text-cyan-400 hover:underline"
+                >
+                  PDF file
+                </a>
+              </div>
+            </div>
+          )}
+          {!fileUrl && !isEditing && (
+            <p
+              className={cn(
+                "text-md text-slate-600 dark:text-slate-300",
+                deleted &&
+                  "italic text-slate-500 dark:text-slate-400 text-xs mt-1"
+              )}
+            >
+              {content}
+              {isUpdated && !deleted && (
+                <span className="text-[10px] mx-2 text-slate-500 dark:text-slate-400">
+                  (edited)
+                </span>
+              )}
+            </p>
+          )}
         </div>
       </div>
+      {canDeleteMessage && (
+        <div
+          className="hidden group-hover:flex items-center gap-x-2
+        absolute p-1 -top-2 right-5 bg-white dark:bg-slate-900 border rounded-sm"
+        >
+          {canEditMessage && (
+            <ActionTootip label="Edit">
+              <Edit2Icon
+                className="cursor-pointer ml-auto w-4 h-4 text-slate-500 hover:text-slate-700 
+              dark:hover:text-slate-300 transition"
+              />
+            </ActionTootip>
+          )}
+          <ActionTootip label="Delete">
+            <Trash2Icon
+              className="cursor-pointer ml-auto w-4 h-4 text-slate-500 hover:text-slate-700 
+              dark:hover:text-slate-300 transition"
+            />
+          </ActionTootip>
+        </div>
+      )}
     </div>
   );
 };
